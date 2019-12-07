4893b9ebd1ed0a5334cc0c6328d83509f3e24b3b
Louisa May Jasmine G. Miguel <40155629+meimeiiiii@users.noreply.github.com>
2019-12-07 16:33:03 +0800

    Add files via upload

---
+++ Calculator/Calculator.vb
@@ -0,0 +1,25 @@
+﻿Public Class Calculator
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+    End Sub
+
+    Private Sub TableLayoutPanel2_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel2.Paint
+
+    End Sub
+
+    Private Sub Button37_Click(sender As Object, e As EventArgs) Handles Button37.Click
+
+    End Sub
+End Class
